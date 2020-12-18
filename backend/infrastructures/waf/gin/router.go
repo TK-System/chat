14845a572c7c9d912829aa300043eafb8728734f
kyota <kyota.helloworld@gmail.com>
2020-12-19 08:50:35 +0900

    change pc

--- backend/infrastructures/waf/gin/router.go
+++ backend/infrastructures/waf/gin/router.go
@@ -2,6 +2,8 @@ package gin
 
 import (
 	"net/http"
+	"io/ioutil"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,6 +25,9 @@ func NewRouter(port string)*Router{
 func (r *Router)setRouter(){
 	r.GET("/",MessageHandler)
 	r.GET("/user")
+	r.GET("/login/",LoginHandler)
+	r.POST("/friendList/",FriendListHandler)
+
 }
 
 func (r *Router)RunServer()error{
@@ -35,4 +40,30 @@ func MessageHandler(c *gin.Context){
 	c.JSON(http.StatusOK,gin.H{
 		"message":"hello world",
 	})
+}
+
+func LoginHandler(c *gin.Context){
+	res := fileRead("/home/ampamman/go/src/chat/frontend/login.html")
+	fmt.Println("\n",c.PostForm("mail"),"\n",c.PostForm("password"))
+	c.Writer.WriteString(
+		res,
+	)
+}
+
+func FriendListHandler(c *gin.Context){
+	res := fileRead("/home/ampamman/go/src/chat/frontend/friendList.html")
+	fmt.Println("\n",c.PostForm("mail"),"\n",c.PostForm("password"))
+	fmt.Println(c)
+	c.Writer.WriteString(
+		res,
+	)
+}
+
+func fileRead(path string)string{
+	bytes, err := ioutil.ReadFile(path)
+    if err != nil {
+        panic(err)
+    }
+
+	return string(bytes)
 }
--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/stretchr/testify v1.6.1 // indirect
 	golang.org/x/crypto v0.0.0-20201112155050-0c6587e931a9 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 )
